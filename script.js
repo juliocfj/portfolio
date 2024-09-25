5f0702bac38a84e707a3319bcdcb2bb2ed0ae86d
Julio Junior <101662786+juliocfj@users.noreply.github.com>
2024-09-24 21:32:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-function showSection(sectionId) {
+function toggleSection(sectionId) {
     const sections = document.querySelectorAll('.content-section');
     sections.forEach(section => {
         section.style.display = 'none';
     });
     document.getElementById(sectionId).style.display = 'block';
+}
+
+function showContent(contentId) {
+    const hiddenContents = document.querySelectorAll('.hidden-content');
+    hiddenContents.forEach(content => {
+        content.style.display = 'none';
+    });
+    document.getElementById(contentId).style.display = 'block';
 }
